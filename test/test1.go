0548532c55d8761b60a3b2b88c37c74aa99a7428
liuyang <mengqing20101212@163.com>
2023-07-02 18:53:37 +0800

    init project

---
+++ test/test1.go
@@ -0,0 +1,17 @@
+package main
+
+import "time"
+
+func main() {
+	go test1()
+	for i := 0; i < 100; i++ {
+		println("hello")
+	}
+	time.Sleep(10)
+}
+
+func test1() {
+	for i := 0; i < 10; i++ {
+		println("word")
+	}
+}
